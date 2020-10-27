1943108ede723c1fcca73d3405c3ecf2b4e48151
NChernetsov <n.chernetsov86@gmail.com>
2020-10-27 12:12:04 +0300

    Задача 1: уточнение решения

--- 1/main.cpp
+++ 1/main.cpp
@@ -1,20 +1,16 @@
 #include <iostream>
-#include <cassert>
 
 int main() {
-    int input;
+    unsigned int input;
     std::cout << "Please enter an integer number (>= 0): " << std::endl;
     std::cin >> input;
-    assert(input >= 0);
 
-    int sum = 0;
-    for (int i = 0; i <= input; i++) {
-        if (i % 3 == 0 and i % 5 == 0) {
+    unsigned long long sum = 0;
+    for (unsigned int i = 3; i < input; ++i) {
+        if (i % 3 == 0 || i % 5 == 0) {
             sum += i;
         }
     }
 
     std::cout << "sum = " << sum << std::endl;
-
-    return 0;
 }
