daa72fa4fea88f3850bb51dee39833f820695900
NChernetsov <n.chernetsov86@gmail.com>
2020-10-28 10:55:28 +0300

    Задача 4: улучшение проверки на простоту

--- 4/main.cpp
+++ 4/main.cpp
@@ -1,27 +1,29 @@
 #include <iostream>
 #include <numeric>
 
-bool is_prime(unsigned int x) {
-    for (int i = 2; i < x; ++i) {
-        if (x % i == 0)
-            return false;
+bool is_prime(int const num) {
+    if (num <= 3) { return num > 1; }
+    else if (num % 2 == 0 || num % 3 == 0) {
+        return false;
+    } else {
+        for (int i = 5; i * i <= num; i += 6) {
+            if (num % i == 0 || num % (i + 2) == 0) {
+                return false;
+            }
+        }
+        return true;
     }
-    return true;
 }
 
 int main() {
-    unsigned int input;
+    int input;
     std::cout << "Enter an integer number >= 0: " << std::endl;
     std::cin >> input;
 
-    unsigned int max_prime = 0;
-
-    for (unsigned int i = input - 1; i >= 1; --i) {
+    for (int i = input - 1; i >= 1; --i) {
         if (is_prime(i)) {
-            max_prime = i;
-            break;
+            std::cout << "max prime < input = " << i << std::endl;
+            return 0;
         }
     }
-
-    std::cout << "max prime < input = " << max_prime << std::endl;
 }
