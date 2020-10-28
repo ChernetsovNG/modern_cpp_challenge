5b052bd2baf15486c12cbb4987693ab4a031f6f2
NChernetsov <n.chernetsov86@gmail.com>
2020-10-28 10:37:55 +0300

    Задача 3: использована функция lcm из std

--- 3/main.cpp
+++ 3/main.cpp
@@ -8,9 +8,5 @@ int main() {
     std::cout << "Enter a second integer number (>= 0): " << std::endl;
     std::cin >> x2;
 
-    unsigned int gcd = std::gcd(x1, x2);
-
-    unsigned int lcm = x1 * x2 / gcd;
-
-    std::cout << "lcm = " << lcm << std::endl;
+    std::cout << "lcm = " << std::lcm(x1, x2) << std::endl;
 }
