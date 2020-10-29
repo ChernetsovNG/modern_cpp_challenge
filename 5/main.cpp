d66fe1b835d8c20cf0c20f584b83d0d9bcb889d7
NChernetsov <n.chernetsov86@gmail.com>
2020-10-29 13:02:47 +0300

    Задача 5: small refactor

--- 5/main.cpp
+++ 5/main.cpp
@@ -1,19 +1,4 @@
 #include <iostream>
-#include <numeric>
-
-bool is_prime(int const num) {
-    if (num <= 3) { return num > 1; }
-    else if (num % 2 == 0 || num % 3 == 0) {
-        return false;
-    } else {
-        for (int i = 5; i * i <= num; i += 6) {
-            if (num % i == 0 || num % (i + 2) == 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-}
 
 int main() {
     int input;
@@ -30,6 +15,7 @@ int main() {
         if (!primes_array[p]) {
             continue;
         }
+        // отмечаем все кратные числа как составные
         for (int k = 2; k * p <= input; ++k) {
             primes_array[p * k] = false;
         }
@@ -37,13 +23,11 @@ int main() {
 
     // все числа, для которые в массиве осталось true - простые
     int index = 1;
-    for (int i = 1; i < input + 1; ++i) {
-        if (primes_array[i] and primes_array[i + 6] and i + 6 < input + 1) {
+    for (int i = 1; i < input - 5; ++i) {
+        if (primes_array[i] and primes_array[i + 6]) {
             std::cout << "sexy primes numbers[" << index++ << "]: " << i << " and " << i + 6 << std::endl;
         }
     }
 
     delete[] primes_array;
-
-    return 0;
 }
