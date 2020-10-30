b8ce27f0274e6054d6afcd676d93edf633aed663
NChernetsov <n.chernetsov86@gmail.com>
2020-10-30 19:10:48 +0300

    Задача 9: улучшение

--- 9/main.cpp
+++ 9/main.cpp
@@ -1,51 +1,31 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
+#include <iterator>
+
+std::vector<unsigned long long> prime_factors(unsigned long long n) {
+    std::vector<unsigned long long> factors;
+    while (n % 2 == 0) {
+        factors.push_back(2);
+        n /= 2;
+    }
+    for (unsigned long long i = 3; i <= std::sqrt(n); i += 2) {
+        while (n % i == 0) {
+            factors.push_back(i);
+            n /= i;
+        }
+    }
+    if (n > 2) {
+        factors.push_back(n);
+    }
+    return factors;
+}
 
 int main() {
-    int input;
-    std::cout << "Enter an integer number >= 0: " << std::endl;
-    std::cin >> input;
+    unsigned long long number = 0;
+    std::cout << "number:";
+    std::cin >> number;
 
-    // массив для реализации решета Эратосфена
-    bool *primes_array = new bool[input + 1];
-    for (int i = 0; i < input + 1; ++i) {
-        primes_array[i] = true;
-    }
-    primes_array[0] = false;
-    for (int p = 2; p * p <= input; ++p) {
-        if (!primes_array[p]) {
-            continue;
-        }
-        // отмечаем все кратные числа как составные
-        for (int k = 2; k * p <= input; ++k) {
-            primes_array[p * k] = false;
-        }
-    }
-    // все числа, для которые в массиве осталось true - простые
-    if (primes_array[input]) {  // само число простое
-        std::cout << input << " is prime number itself" << std::endl;
-        return 0;
-    }
-
-    int m = input;
-    bool first = true;
-
-    std::cout << m << " = ";
-    for (int i = 2; i < input && m > 1; ++i) {
-        if (!primes_array[i]) {
-            continue;
-        }
-        while (m % i == 0) {
-            if (first) {
-                std::cout << i;
-                first = false;
-            } else {
-                std::cout << " * " << i;
-            }
-            m /= i;
-        }
-    }
-    std::cout << std::endl;
-
-    delete[] primes_array;
+    auto factors = prime_factors(number);
+    std::copy(std::begin(factors), std::end(factors), std::ostream_iterator<unsigned long long>(std::cout, " "));
 }
