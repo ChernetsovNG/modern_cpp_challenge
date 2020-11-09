c7a9833cc2e236e857c5986e8430fc8d671d4bd7
NChernetsov <n.chernetsov86@gmail.com>
2020-11-09 13:28:18 +0300

    Задача 11: более оптимальное решение

--- 11/main.cpp
+++ 11/main.cpp
@@ -3,68 +3,29 @@
 #include <algorithm>
 #include <string>
 
-std::string to_roman_helper(int n, int k) {
-    std::vector<char> v;
-
-    if (k == 3) {
-        for (int i = 0; i < n; ++i) {
-            v.push_back('M');
-        }
-    } else {
-        char A, B, C;
-        if (k == 0) {
-            A = 'I';
-            B = 'V';
-            C = 'X';
-        } else if (k == 1) {
-            A = 'X';
-            B = 'L';
-            C = 'X';
-        } else if (k == 2) {
-            A = 'C';
-            B = 'D';
-            C = 'M';
-        }
-        if (n >= 0 && n <= 3) {
-            for (int i = 0; i < n; ++i) {
-                v.push_back(A);
-            }
-        } else if (n >= 4 && n <= 8) {
-            if (n > 5) {
-                for (int i = 0; i < n - 5; ++i) {
-                    v.push_back(A);
-                }
-                v.push_back(B);
-            } else {
-                v.push_back(B);
-                for (int i = 0; i < 5 - n; ++i) {
-                    v.push_back(A);
-                }
-            }
-        } else if (n == 9) {
-            v.push_back(C);
-            v.push_back(A);
+std::string to_roman(unsigned int value) {
+    std::vector<std::pair<unsigned int, char const *>> roman{
+            {1000, "M"},
+            {900,  "CM"},
+            {500,  "D"},
+            {400,  "CD"},
+            {100,  "C"},
+            {90,   "XC"},
+            {50,   "L"},
+            {40,   "XL"},
+            {10,   "X"},
+            {9,    "IX"},
+            {5,    "V"},
+            {4,    "IV"},
+            {1,    "I"}};
+    std::string result;
+    for (auto const &kvp : roman) {
+        while (value >= kvp.first) {
+            result += kvp.second;
+            value -= kvp.first;
         }
     }
-
-    std::reverse(v.begin(), v.end());
-    return std::string(v.begin(), v.end());
-}
-
-std::string to_roman(int x) {
-    int n0, n1, n2, n3;
-
-    n0 = x % 10;
-    n1 = (x % 100) / 10;
-    n2 = (x % 1000) / 100;
-    n3 = x / 1000;
-
-    const std::string &r3 = to_roman_helper(n3, 3);
-    const std::string &r2 = to_roman_helper(n2, 2);
-    const std::string &r1 = to_roman_helper(n1, 1);
-    const std::string &r0 = to_roman_helper(n0, 0);
-
-    return r3 + r2 + r1 + r0;
+    return result;
 }
 
 int main() {
