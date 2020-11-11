a14398c7ee23a2556c7fd3846f2072446f705b62
NChernetsov <n.chernetsov86@gmail.com>
2020-11-11 11:34:28 +0300

    Задача 13: вычисление числа pi через сумму Лейбница

--- 13/main.cpp
+++ 13/main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
-#include <string>
 
 int main() {
-    int x;
+    double eps = 1e-4;
+
+    double sum = 1.0;
+    double delta = 1.0;
+
+    int i = 1;
+    while (delta > eps) {
+        int term = i % 2 == 0 ? 1 : -1;
+        double next = term * 1.0 / (2 * i + 1);
+
+        double sum_next = sum + next;
+        delta = std::abs(sum - sum_next);
+        sum = sum_next;
+
+        i++;
+    }
+
+    double pi = sum * 4;
+    std::cout << "pi = " << pi << std::endl;
+    std::cout << "count = " << i << std::endl;
 }
